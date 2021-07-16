e1c7234d171fca12859b19eec789d8badf047f5c
Alfredo Cabrera <acabrera@programatio.com>
2021-07-16 13:22:08 -0500

    My first project with mix

---
+++ hola_mundo/lib/hola_mundo.ex
@@ -0,0 +1,18 @@
+defmodule HolaMundo do
+  @moduledoc """
+  Documentation for `HolaMundo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HolaMundo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
